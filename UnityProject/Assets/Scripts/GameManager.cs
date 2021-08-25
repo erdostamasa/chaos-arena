b65b57ba181e7d9e5a4cc9f7aa8a7a03116f45ec
Erdős Tamás <62765033+erdostamasa@users.noreply.github.com>
2021-08-25 14:01:05 +0200

    Improved aiming

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -16,14 +16,14 @@ public class GameManager : MonoBehaviour {
     [SerializeField] PlatformSpawner platformSpawner;
     public int money = 0;
 
-    //[SerializeField] float spawnFrequency = 5f;
+    [SerializeField] float spawnFrequency = 5f;
     float timer;
     void Update() {
-       /* timer += Time.deltaTime;
+        timer += Time.deltaTime;
         if (timer >= spawnFrequency) {
             platformSpawner.SpawnPlatform();
             timer = 0;
-        }*/
+        }
     }
 
     void Start()
--- UnityProject/Assets/Scripts/WeaponController.cs
+++ UnityProject/Assets/Scripts/WeaponController.cs
@@ -26,20 +26,22 @@ public class WeaponController : MonoBehaviour {
         }
         
         
-        //Plane plane = new Plane(Vector3.up, -player.position.y);
+        Plane plane = new Plane(Vector3.up, -1);
 
         float distance;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out distance);
         RaycastHit hit;
         //Debug.DrawRay(ray.origin, ray.direction * 50f, Color.blue);
-        if (Physics.Raycast(ray, out hit, 100f, targetingMask)) {
-            //targeter.position = ray.GetPoint(distance);
+        //if (Physics.Raycast(ray, out hit, 100f, targetingMask)) {
+        if (plane.Raycast(ray, out distance)) {
+            targeter.position = ray.GetPoint(distance);
 
-            Debug.Log(hit.collider.name);
+            //Debug.Log(hit.collider.name);
             //Smoothly move to mouse position
-            Vector3 directionToMove = hit.point;
+            //Vector3 directionToMove = hit.point;
             //float distanceToMove = (directionToMove - targeter.position).sqrMagnitude;
-            targeter.position += (directionToMove - targeter.position) * Time.deltaTime * 50f;
+            //targeter.position += (directionToMove - targeter.position) * Time.deltaTime * 50f;
             //targeter.position = new Vector3(targeter.position.x, player.position.y, targeter.position.z);
 
             
